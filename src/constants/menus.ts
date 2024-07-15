c02e5399320d675d100052093f49265fd1e039c6
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-14 21:06:34 -0700

    Constantes para los logos

---
+++ src/constants/menus.ts
@@ -0,0 +1,6 @@
+export const Logo = {
+    Light: "/img/InstaPoor.webp",
+    Dark: "/img/InstaPoor_white.webp",
+    SmallLight: "/img/InstaPoor_short.webp",
+    SmallDark: "/img/InstaPoor_short_white.webp"
+}
