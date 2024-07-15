c265a830dad922410a63be9b80acb023c253bc25
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-14 21:06:44 -0700

    se termino el Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,18 @@
+"use client"
+import Image from 'next/image';
+import { Logo } from '@/constants/menus';
+import ChangeTheme from '@/service/ChangeTheme';
+import style from "@/style/header.module.css"
+import Input from './Input';
+
 export default function Header() {
-    return <header> Hola</header>
+    const {colorScheme} = ChangeTheme()
+    return <header className={style.header}> 
+        <Image src={colorScheme == "light" ? Logo.SmallLight : Logo.SmallDark} alt="Logo de instapoor corto" width={32}  height={32}/>
+        <Image src={colorScheme == "light" ? Logo.Light : Logo.Dark} alt="Logo de instapoor" width={136}  height={32}/>
+
+        <div>
+            <Input placeholder='Buscar' className={style.input}/>        
+        </div>
+    </header>
 }
