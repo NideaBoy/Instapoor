d6b2813d1149df44ee2260361198669d55b458a5
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-20 09:28:21 -0700

    Se hizo el menu de las historias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import Image from "next/image";
-import styles from "@/style/page.module.css";
+import { Inter } from "next/font/google";
+import styles from "@/style/Home.module.css";
+import { cnn } from "@/service/cnn";
+import Profile from "@/interface/IProfile";
+import HeaderHistory from "@/components/HeaderHistory";
 
-export default function Home() {
+const inter = Inter({
+  display: "block",
+  subsets: ["latin"],
+  weight: ["400"]
+})
+
+export default async function Home() {
+  const res = await cnn.get("/users")
+
+  console.log(res.data[0].id.machine)
   return (
-    <main className={styles.main}>
-     
+    <main>
+      <HeaderHistory>
+        <ul className={styles.history__list}>
+          {res.data.map((i: Profile) =>
+            <li key={crypto.randomUUID()}>
+              <a href={`#history/${i.identify}`} title={i.username} className={`${styles.history__link} ${i.history ? styles["history__link-history"] : ""}`}>
+                <img src={`${process.env.API_URL}${i.picture}`} alt={i.username} />
+                <span className={inter.className}>{i.username}</span>
+              </a>
+            </li>
+          )}
+        </ul>
+      </HeaderHistory>
     </main>
   );
 }
