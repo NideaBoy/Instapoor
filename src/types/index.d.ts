8a7be1a4d51741e48c058706cc02a69b50108a2a
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-20 09:31:29 -0700

    se agregaron tipos al process.env

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,13 @@ declare module '*.module.css' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      [key: string]: string | undefined;
+      API_URL: string;
+      // add more environment variables and their types here
+    }
+  }
+}
