c16365fc0cb25597fdee42a6a6887331cbaa10e4
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-21 21:15:10 -0700

    Se cambio en icono del sol
    Se cambio el icono de sol y se formateo el archivo

--- src/components/MenuAside.tsx
+++ src/components/MenuAside.tsx
@@ -4,8 +4,7 @@ import { Inter } from 'next/font/google';
 import { usePathname } from 'next/navigation'
 import { AiFillHome, AiOutlineSearch, AiOutlineCompass, AiOutlineHome, AiFillCompass } from "react-icons/ai";
 import { IoIosCreate } from "react-icons/io";
-import { CiSun } from "react-icons/ci";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import Image from 'next/image';
 import Input from './Input';
 import { Logo } from '@/constants/menus';
@@ -18,7 +17,7 @@ export default function Aside() {
     const pathname = usePathname()
     const [isOpen, onOpen] = useState(false)
 
-    const {colorScheme, toggleColorScheme} = ChangeTheme()
+    const { colorScheme, toggleColorScheme } = ChangeTheme()
 
     function active(path: string, name: string = "sidebar__link-") {
         if (path === pathname)
@@ -29,14 +28,14 @@ export default function Aside() {
         <aside className={`${style.sidebar} ${isOpen ? style["sidebar-close"] : ""} ${inter.className}`}>
             <header className={style.sidebar__header}>
                 <a href="/" title='Logo de instapoor'>
-                <picture className="logo__small">
-                        <Image src={colorScheme == "light" ? Logo.SmallLight : Logo.SmallDark} alt="Logo de instapoor corto" width={32}  height={32} priority={true}/>
-                </picture>
-                <picture className={`logo__small ${isOpen ? 'logo__small-close' : ""}`}>
-                        <Image src={colorScheme == "light" ? Logo.SmallLight : Logo.SmallDark} alt="Logo de instapoor corto" width={32}  height={32} priority={true}/>
-                </picture>
+                    <picture className="logo__small">
+                        <Image src={colorScheme == "light" ? Logo.SmallLight : Logo.SmallDark} alt="Logo de instapoor corto" width={32} height={32} priority={true} />
+                    </picture>
+                    <picture className={`logo__small ${isOpen ? 'logo__small-close' : ""}`}>
+                        <Image src={colorScheme == "light" ? Logo.SmallLight : Logo.SmallDark} alt="Logo de instapoor corto" width={32} height={32} priority={true} />
+                    </picture>
                     <picture className={`logo ${isOpen ? 'logo-close' : ""}`}>
-                        <Image src={colorScheme == "light" ? Logo.Light : Logo.Dark} alt="Logo de instapoor" width={136}  height={32} priority={true}/>
+                        <Image src={colorScheme == "light" ? Logo.Light : Logo.Dark} alt="Logo de instapoor" width={136} height={32} priority={true} />
                     </picture>
                 </a>
             </header>
@@ -49,20 +48,20 @@ export default function Aside() {
                         </a>
                     </li>
                     <li className={style.sidebar__item}>
-                        <button className={`${style.sidebar__link} ${isOpen ? "clicked" : ""}`} onClick={()=>onOpen(!isOpen)}>
-                            <i className={style.sidebar__icon}><AiOutlineSearch  /></i>
+                        <button className={`${style.sidebar__link} ${isOpen ? "clicked" : ""}`} onClick={() => onOpen(!isOpen)}>
+                            <i className={style.sidebar__icon}><AiOutlineSearch /></i>
                             <span>Buscar</span>
                         </button>
                     </li>
                     <li className={style.sidebar__item}>
                         <button className={style.sidebar__link}>
-                            <i className={style.sidebar__icon}><IoIosCreate  /></i>
+                            <i className={style.sidebar__icon}><IoIosCreate /></i>
                             <span>Crear</span>
                         </button>
                     </li>
                     <li className={style.sidebar__item}>
                         <a href="/explore" className={`${style.sidebar__link} ${active("/explore")}`}>
-                            {pathname === "/explore" ? <i className={style.sidebar__icon}><AiFillCompass  /></i>  :  <i><AiOutlineCompass className={style.sidebar__icon} /></i>}
+                            {pathname === "/explore" ? <i className={style.sidebar__icon}><AiFillCompass /></i> : <i><AiOutlineCompass className={style.sidebar__icon} /></i>}
 
                             <span>explorar</span>
                         </a>
@@ -76,18 +75,18 @@ export default function Aside() {
                 </ul>
             </nav>
             <button className={`${style.btn__system} ${inter.className}`} onClick={toggleColorScheme}>
-                <i>{colorScheme === 'light' ? <CiSun/>: <FaMoon/>}</i> <span>Cambiar apariencia</span>
+                <i>{colorScheme === 'light' ? <FaSun /> : <FaMoon />}</i> <span>Cambiar apariencia</span>
             </button>
         </aside>
-            <div className={`${style.sidebar__search} ${isOpen ? style["sidebar__search-open"] : ""}`}>
-                <form role='search' className={style.form__search}>
-                    <h1 className={`${style.search__title} ${inter.className}`}>Buscar</h1>
-                    <Input placeholder='Buscar' type='text' className={style.input}/>
-                </form>
-                <h4 className={`${style.alredy__title} ${inter.className}`}>Recientes</h4>
-                <div className={`${style.container__alredy} ${inter.className}`}>
-                    <p>No busqueda reciente.</p>
-                </div>
+        <div className={`${style.sidebar__search} ${isOpen ? style["sidebar__search-open"] : ""}`}>
+            <form role='search' className={style.form__search}>
+                <h1 className={`${style.search__title} ${inter.className}`}>Buscar</h1>
+                <Input placeholder='Buscar' type='text' className={style.input} />
+            </form>
+            <h4 className={`${style.alredy__title} ${inter.className}`}>Recientes</h4>
+            <div className={`${style.container__alredy} ${inter.className}`}>
+                <p>No busqueda reciente.</p>
             </div>
+        </div>
     </>)
 }
