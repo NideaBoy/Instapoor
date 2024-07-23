7c58418edc54c805e6f07375c37482d791034705
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-07-22 19:14:10 -0700

    Se cambio los nombre de las interfaces

--- src/interface/IProfile.ts
+++ src/interface/IProfile.ts
@@ -1,12 +1,12 @@
-export default interface Profile {
+export default interface IProfile {
     id: any
     username: string
     identify: string
     picture: string
-    history?: History
+    history?: THistory
 }
 
-type History = {
+type THistory = {
     id: string
     like: number
     video: any
