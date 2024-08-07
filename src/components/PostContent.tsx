f2c4ef69df5207b62e4a26b02970233f5a28605f
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2024-08-07 16:36:31 -0700

    se uso un cnn personalizado

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,23 +1,23 @@
 "use client"
+import { useEffect, useState, useRef } from "react"
 import IPost from "@/interface/IPost"
-import { cnn } from "@/service/cnn"
-import { useEffect, useState } from "react"
+import { cnn } from "@/service/cnnClient"
 
 import Post from "./Post"
+import Loading from "./Loading"
 export default function PostContent() {
-    const [post, setPost] = useState<IPost[]>([])
-    const [limit, setLimit] = useState(3)
 
-    useEffect(() => {
-        async function getPost() {
-            const res = await cnn.get<IPost[]>(`/post/content/${limit}`)
-            setPost(res.data)
-        }
-        getPost()
-    }, [])
+    const [limit, setLimit] = useState(3)
+    const scroll = useRef<HTMLDivElement>(null)
+
+    const res = cnn<IPost>(`/post/content/${limit}`, {
+        method: "get",
+        body: null
+    })
+    // if (res.isLoading) return <Loading />
     return (
-        <div>
-            {post.map((i) => <Post key={crypto.randomUUID()} alt={i.alt} content={i.content} user={i.user} />
+        <div ref={scroll}>
+            {res.data.map((i) => <Post key={crypto.randomUUID()} alt={i.alt} content={i.content} user={i.user} />
             )}
         </div>
     )
